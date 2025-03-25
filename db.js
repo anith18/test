ea3e6bb687955441f6de730ff163431ea3dd2578
anith18 <anithraju174@gmail.com>
2025-03-25 18:33:02 +0530

    latest

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb://127.0.0.1:27017/inotebook'; // Use 127.0.0.1 instead of localhost to avoid IPv6 issues.
+const mongoURI = 'mongodb+srv://anith:anith123@cluster0.jrhtr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'; // Use 127.0.0.1 instead of localhost to avoid IPv6 issues.
 
 const connectToMongo = async () => {
   try {
--- index.js
+++ index.js
@@ -12,13 +12,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
+    origin:  "https://frontend-k1j5.onrender.com",
     methods: "GET, POST, PUT, DELETE",
     credentials: true, // Allow cookies and authentication headers
 }));
